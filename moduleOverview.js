6125c07f5da0935b3f5d30273a841918de551f55
poroshhossain <poroshhossain90@gmail.com>
2025-08-02 05:43:17 +0600

    javascriptInterviewQuestion set up

---
+++ moduleOverview.js
@@ -0,0 +1,13 @@
+
+//01.  null vs undefined in javascript
+// In JavaScript, null and undefined both represent the absence of a value, but they differ in their origin and intended use
+
+
+
+// 02. when = and == or === using
+
+
+// 03. what is this keyword
+
+// 04. what is problem argument javascript
+
